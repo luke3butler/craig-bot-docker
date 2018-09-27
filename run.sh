29cd02db26bffc87ff6549365a0b57740e6a5775
Juuso Vilmunen <juuso@vilmunen.net>
2018-09-27 09:08:38 +0300

    commandline parameters for urls and nickname

--- run.sh
+++ run.sh
@@ -2,10 +2,21 @@
 set -e
 
 BOT_TOKEN=$1
+NICK=$2
+URL=$3
+DLURL=$4
+
+echo PARAMS $*
 
 sed -i -e "s/BOT_TOKEN/${BOT_TOKEN}/g" config.json
+sed -i -e "s/NICK/${NICK}/g" config.json
+sed -i -e "s/HOME_URL/${URL}/g" config.json
+sed -i -e "s/DL_URL/${DLURL}/g" config.json
 
 cat config.json
+cat craig/default-config.js
 
-bash craig.sh
+mkdir rec
+
+node craig-runner.js
 
